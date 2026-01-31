fc1a494fda43677292152c00f13bcca50421e828
giovani-leite <giovanileitearaujo@gmail.com>
2026-01-31 18:09:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const fadeElems = document.querySelectorAll('.fade-in');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); // Animates only once
+            }
+        });
+    }, observerOptions);
+
+    fadeElems.forEach(elem => {
+        observer.observe(elem);
+    });
+});
